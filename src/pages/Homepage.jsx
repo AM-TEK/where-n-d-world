a048db2d4315d9df50999bad0ffa36f68306dda4
Alejandro Maldonado <am@AM-WEBDEV.attlocal.net>
2024-03-18 13:54:30 -0500

    add css modules

--- src/pages/Homepage.jsx
+++ src/pages/Homepage.jsx
@@ -1,13 +1,15 @@
 import { Link } from "react-router-dom"
 import PageNav from "../components/PageNav"
+import AppNav from "../components/AppNav"
 
 export default function Homepage() {
   return (
     <div>
       <PageNav />
+      <AppNav />
       <h1>Where-N-D-World</h1>
       
-      <Link to='/Pricing'>Pricing</Link>
+      <Link to='/app'>Go to the app</Link>
       
     </div>
   )
