f3fcc5149e18fe78ab1e2a20f6e2a993564beffc
Alejandro Maldonado <am@AM-WEBDEV.attlocal.net>
2024-03-21 15:03:34 -0500

    add CounrtyList comp, use reduce to loop over cities array

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,5 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom"
+import { useEffect, useState } from "react"
 import Pricing from "./pages/Pricing"
 import Product from './pages/Product'
 import Login from "./pages/Login"
@@ -6,7 +7,7 @@ import Homepage from "./pages/Homepage"
 import PageNotFound from "./pages/PageNotFound"
 import AppLayout from "./pages/AppLayout"
 import CityList from "./components/CityList"
-import { useEffect, useState } from "react"
+import CountryList from "./components/CountryList"
 
 const BASE_URL = 'http://localhost:9000'
 
@@ -40,7 +41,7 @@ function App() {
         <Route path="app" element={<AppLayout />}>
           <Route index element={<CityList cities={cities} isLoading={isLoading} />} />
           <Route path="cities" element={<CityList cities={cities} isLoading={isLoading} />} />
-          <Route path="countries" element={<p>List of countries</p>} />
+          <Route path="countries" element={<CountryList cities={cities} isLoading={isLoading} />} />
           <Route path="form" element={<p>Form</p>} />
         </Route>
         <Route path="*" element={<PageNotFound />} />
