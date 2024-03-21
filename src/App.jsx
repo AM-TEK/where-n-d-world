6c84e536ed7c435a2a3d691479681bd61e51e0f3
Alejandro Maldonado <am@AM-WEBDEV.attlocal.net>
2024-03-21 16:47:08 -0500

    add route, link and useParams for id in url

--- src/App.jsx
+++ src/App.jsx
@@ -8,6 +8,7 @@ import PageNotFound from "./pages/PageNotFound"
 import AppLayout from "./pages/AppLayout"
 import CityList from "./components/CityList"
 import CountryList from "./components/CountryList"
+import City from "./components/City"
 
 const BASE_URL = 'http://localhost:9000'
 
@@ -37,10 +38,11 @@ function App() {
         <Route index element={<Homepage />} />
         <Route path="pricing" element={<Pricing />} />
         <Route path="product" element={<Product />} />
-        <Route path="/login" element={<Login />} />
+        <Route path="login" element={<Login />} />
         <Route path="app" element={<AppLayout />}>
           <Route index element={<CityList cities={cities} isLoading={isLoading} />} />
           <Route path="cities" element={<CityList cities={cities} isLoading={isLoading} />} />
+          <Route path="cities/:id" element={<City />} />
           <Route path="countries" element={<CountryList cities={cities} isLoading={isLoading} />} />
           <Route path="form" element={<p>Form</p>} />
         </Route>
